dc9fa8ab7f34909c48c168dae628c1e67d0e9b16
Steven Burris <stevenburris.jr@gmail.com>
2016-11-03 10:34:34 -0400

    Woah....cards;

---
+++ src/com/theironyard/Card.java
@@ -0,0 +1,75 @@
+package com.theironyard;
+
+/**
+ * Created by stevenburris on 11/3/16.
+ */
+public class Card {
+    enum Suit {
+        CLUBS,
+        DIAMONDS,
+        HEARTS,
+        SPADES
+    }
+
+    enum Rank {
+        ACE,
+        TWO,
+        THREE,
+        FOUR,
+        FIVE,
+        SIX,
+        SEVEN,
+        EIGHT,
+        NINE,
+        TEN,
+        JACK,
+        QUEEN,
+        KING
+    }
+
+    Suit suit;
+    Rank rank;
+
+    public Card() {
+    }
+
+    public Card(Suit suit, Rank rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+
+    public void setSuit(Suit suit) {
+        this.suit = suit;
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
+
+    public void setRank(Rank rank) {
+        this.rank = rank;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Card card = (Card) o;
+
+        if (suit != card.suit) return false;
+        return rank == card.rank;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = suit.hashCode();
+        result = 31 * result + rank.hashCode();
+        return result;
+    }
+}
---
+++ src/com/theironyard/Main.java
@@ -0,0 +1,49 @@
+package com.theironyard;
+
+import java.util.HashSet;
+
+public class Main {
+
+    public static HashSet<Card> createDeck() {
+        HashSet<Card> deck = new HashSet<>();
+        for (Card.Suit suit : Card.Suit.values()) {
+            for (Card.Rank rank : Card.Rank.values()) {
+                Card c = new Card(suit, rank);
+                deck.add(c);
+            }
+        }
+        return deck;
+    }
+
+    public static HashSet<HashSet<Card>> createHands(HashSet<Card> deck) {
+        HashSet<HashSet<Card>> hands = new HashSet<>();
+        for (Card c1 : deck) {
+            HashSet<Card> deck2 = (HashSet<Card>) deck.clone();
+            deck2.remove(c1);
+            for (Card c2 : deck2) {
+                HashSet<Card> deck3 = (HashSet<Card>) deck2.clone();
+                deck3.remove(c2);
+                for (Card c3 : deck3) {
+                    HashSet<Card> deck4 = (HashSet<Card>) deck3.clone();
+                    deck4.remove(c3);
+                    for (Card c4 : deck4) {
+                        HashSet<Card> hand = new HashSet<>();
+                        hand.add(c1);
+                        hand.add(c2);
+                        hand.add(c3);
+                        hand.add(c4);
+                        hands.add(hand);
+                    }
+                }
+            }
+        }
+        return hands;
+    }
+
+    public static void main(String[] args) {
+        HashSet<Card> deck = createDeck();
+        HashSet<HashSet<Card>> hands = createHands(deck);
+        System.out.println(hands.size());
+
+    }
+}
